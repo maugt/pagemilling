36742d36c10fa5186a8b4f2f52c5b7190e370520
maugt <giles@Giless-MacBook-Pro.local>
2023-11-06 21:42:03 -0800

    syntax

--- pages/riders.js
+++ pages/riders.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Image from 'next/image'
 
 export default function Riders() {
     const [riders, setRiders] = useState([]);
@@ -16,26 +17,32 @@ export default function Riders() {
     let prevCount = null;
     return (
         <div id="container">
-            
+
             <div className="pagetitle">
-            <h1>Page Mill In a Halloween Costume Counter</h1>
+                <h1>Page Mill In a Halloween Costume Counter</h1>
             </div>
             <table className="leaderboard" id="leaderboard">
                 <thead>
-                    <tr><th>Rank</th><th>Photo</th><th>Name</th><th>Costumed Page Mill Count</th><th></th></tr>
-                                    <tr>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Costumed Page Mill Count</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {riders.sort(function(a, b) { return b.count - a.count }).map((rider, index) => (
+                    {riders.sort(function (a, b) { return b.count - a.count }).map((rider, index) => (
                         rank = prevCount != rider.count ? index + 1 : rank,
                         prevCount = rider.count,
-    <tr key={index} className={rank === 1 ? 'mayor' : ''}>
-     <td class="count"><div class={`icon rank${rank}`}></div></td>
-     <td class="photo"><img width="124" height="124" src={`${rider.name}.jpg`}/></td>
-     <td><a href={`https://www.strava.com/athletes/${rider.id}`}>{rider.name}</a></td>
-     <td class="count">{rider.count}</td><td>Details</td>
-                           {/* <td>{rider.name}</td>
+                        <tr key={index} className={rank === 1 ? 'mayor' : ''}>
+                            <td class="count"><div class={`icon rank${rank}`}/></td>
+                            <td class="photo">
+                                <img width="124" height="124" alt={`Phone of ${rider.name} in costume`}
+                                    src={`${rider.name}.jpg`} /></td>
+                            <td><a href={`https://www.strava.com/athletes/${rider.id}`}>{rider.name}</a></td>
+                            <td class="count">{rider.count}</td><td>Details</td>
+                            {/* <td>{rider.name}</td>
                             <td>{rider.count}</td> */}
                         </tr>
                     ))}
